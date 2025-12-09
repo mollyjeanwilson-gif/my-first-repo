bbff13798273576a0e610bb9cd971be9353e0d6f
mollyjeanwilson-gif <mollyjeanwilson@gmail.com>
2025-12-09 16:12:26 +0000

    change background colour

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 function setup() {
     createCanvas(400, 400);
-    background(255);
+    background(0);
 }
 
 function draw() {
+    noStroke();
    fill('white');
     ellipse(mouseX, mouseY, 5);
 }
 
 function mouseClicked(){
-    background(255);
+    background(0);
 }
